c4feb09c0c0d788ecc4ccd5a6f7c1365cd837fc1
hamseungwan2023 <syham001@naver.com>
2023-11-16 21:32:38 +0900

    nono1

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const messageRouter = require("./messageRoute");
+const path = require("path");
+
 // const cors = require("cors");
 // const corsOption = {
 //   origin: ["http://ec2-54-226-163-236.compute-1.amazonaws.com:5000/"],
@@ -16,17 +18,16 @@ app.use(express.json());
 app.use("/api", messageRouter);
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
+  res.sendFile(path.join(__dirname, "/in  dex.html"));
 });
 
 app.set("port", process.env.PORT || 5000);
 const uri = process.env.ATLAS_URI;
 
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기중..");
+});
 mongoose
   .connect(uri)
   .then(() => console.log("MongoDB connection established"))
   .catch((error) => console.log("MongoDB connection failed:", error.message));
-
-app.listen(port, (req, res) => {
-  console.log(`Server running on port: ${port}`);
-});
